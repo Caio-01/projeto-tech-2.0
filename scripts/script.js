97e7875aeeccea972aa2d68530e3319a9acc645d
Caio-01 <caiosantos.costa02@gmail.com>
2023-08-09 17:52:07 -0300

    atualizado

--- scripts/script.js
+++ scripts/script.js
@@ -27,43 +27,6 @@ const toggleMenu = () => {
 };
 
 
-const slider = document.querySelectorAll('.slider');
-const btnPrev = document.getElementById('prev-button');
-const btnNext = document.getElementById('next-button');
 
-let currentSlide = 0;
-
-//Funçoes para passar as imagens com os botoes
-function hideSlider(){
-    slider.forEach(item => item.classList.remove('on'))
-}
-
-function showSlider(){
-    slider[currentSlide].classList.add('on')
-}
-
-function nextSlider(){
-    hideSlider()
-    if(currentSlide == slider.length -1){
-        currentSlide = 0
-    }else {
-        currentSlide++
-    }
-    showSlider()
-}
-
-function prevSlider(){
-    hideSlider()
-    if(currentSlide == 0){
-        currentSlide = slider.length -1
-    }else {
-        currentSlide--
-    }
-    showSlider()
-}
-
-
-btnNext.addEventListener('click', nextSlider);
-btnPrev.addEventListener('click', prevSlider);
 
 
